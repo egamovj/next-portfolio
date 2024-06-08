4a6ee1712b43aa407157f364caecfddb2896f29d
egamovj <egamovj90@gmail.com>
2024-06-08 15:34:24 +0500

    hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,7 +4,7 @@ import HeroSection from "./components/HeroSection";
 
 export default function page() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-black overflow-hidden">
       <div className="max-w-7xl mx-auto p-5">
         <Navbar />
         <HeroSection />
