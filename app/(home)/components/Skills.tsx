3ad40f1327287a29c9ff82fac39521061e611a94
egamovj <egamovj90@gmail.com>
2024-06-08 17:35:18 +0500

    skills section completed

--- app/(home)/components/Skills.tsx
+++ app/(home)/components/Skills.tsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import Title from './Title'
+'use client'
+import React from "react";
+import Title from "./Title";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import {
+  SiGit,
+  SiJavascript,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiReact,
+  SiSupabase,
+  SiSvelte,
+  SiTailwindcss,
+} from "react-icons/si";
 
 export default function Skills() {
+  const skills = [
+    {
+      text: "React",
+      Icon: SiReact,
+    },
+    {
+      text: "Next.js",
+      Icon: SiNextdotjs,
+    },
+    {
+      text: "Tailwind",
+      Icon: SiTailwindcss,
+    },
+    {
+      text: "Supabase",
+      Icon: SiSupabase,
+    },
+    {
+      text: "Node.js",
+      Icon: SiNodedotjs,
+    },
+    {
+      text: "Svelte",
+      Icon: SiSvelte,
+    },
+    {
+      text: "Javascript",
+      Icon: SiJavascript,
+    },
+    {
+      text: "Git",
+      Icon: SiGit,
+    },
+  ];
+
   return (
-    <div>
-      <Title text="Skills 🔪" className="flex flex-col items-center justify-center -rotate-6" />
+    <div className="max-w-5xl mx-auto px-8">
+      <Title
+        text="Skills 🔪"
+        className="flex flex-col items-center justify-center -rotate-6"
+      />
+      <HoverEffect items={skills} />
     </div>
-  )
+  );
 }
